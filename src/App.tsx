3ddbc9e5ce918a8483a9633b40037d4dd4138a7d
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-04-09 12:13:07 +0800

    Add more theme options

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Layout } from 'antd';
 import Counter from './components/Counter';
 import './App.css';
 import 'antd/dist/antd.min.css';
+import { useStores } from './stores';
+import { themes } from './constants/theme';
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
+  const { preference } = useStores()
+  const theme = themes[preference.activeTheme]
   return (
     <Layout>
-      <Header style={{ textAlign: "center", color: "white", fontWeight: "bold" }}>Three to One</Header>
+      <Header style={{ textAlign: "center", ...theme.header }}><span style={{ color: theme.highlight }}>Three</span> to One</Header>
       <Content>
         <Counter />
       </Content>
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,9 +2,25 @@ export const themes = {
   dark: {
     color: "white",
     highlight: "#91F291",
+    secondary: "#F2F230",
+    warn: "#C2F261",
+    blue1: "61F2C2",
+    blue2: "30F2F2",
+    header: {
+      color: "white",
+      fontWeight: "bold",
+    },
   },
   light: {
     color: "inherit",
     highlight: "#91F291",
+    secondary: "#F2F230",
+    warn: "#C2F261",
+    blue1: "61F2C2",
+    blue2: "30F2F2",
+    header: {
+      color: "white",
+      fontWeight: "bold",
+    },
   }
 }
